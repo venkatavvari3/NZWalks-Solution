bce5b43fdf28ef529024bfe6724da25c9b7e7dcd
venkatavvari3 <venkat.avvari3@gmail.com>
2023-07-27 12:54:23 +0100

    Implement Exception Handling

--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.OpenApi.Models;
 using NZWalks.API.Data;
 using NZWalks.API.Mappings;
 using NZWalks.API.Repositories;
+using Serilog;
 using System.Text;
 
 namespace NZWalks.API
@@ -19,6 +20,14 @@ namespace NZWalks.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
+            /*var logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File("Logs/NzWalks_log.txt", rollingInterval: RollingInterval.Minute)
+                .MinimumLevel.Warning()
+                .CreateLogger();
+            builder.Logging.ClearProviders();
+            builder.Logging.AddSerilog(logger);*/
 
             builder.Services.AddControllers();
             builder.Services.AddHttpContextAccessor();
